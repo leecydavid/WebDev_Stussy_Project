60b8efc69dc2deac86caefe9dd975cebda62497a
leecydavid <leecydavid@gmail.com>
2023-03-03 10:48:13 -0800

    Updated Java Script

---
+++ stussywebsite/script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// window.onclick = function(event) {
+//     if (!event.target.matches(".shop")) {
+//         var menudrop = document.getElementsById("myDropdown");
+//         var i;
+//         for (var i=0; i < menudrop.length; i++) {
+//             var openDropdown = menudrop [i];
+//             if (openDropdown.classlist.contains('show')) {
+//                 openDropdown.classlist.remove('show');
+//             }
+//         }
+//     }
+// }
+
